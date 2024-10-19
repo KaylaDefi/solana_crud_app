2f0d44cf73e4c87180b89c3d1e857e2532a5aaa8
KaylaDefi <ksherwo1@outlook.com>
2024-10-18 22:38:36 -0600

    manually calculated space

--- anchor/programs/crudapp/src/lib.rs
+++ anchor/programs/crudapp/src/lib.rs
@@ -1,10 +1,6 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
-use dotenv::dotenv; 
-use std::env; 
-
-dotenv().ok();
 
 declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
@@ -13,75 +9,96 @@ pub mod crudapp {
     use super::*;
 
     pub fn create_journal_entry(ctx: Context<CreateEntry>, title: String, message: String) -> Result<()> {
-      let journal_entry = &mut ctx.accounts.journal_entry;
-      journal_entry.owner = *ctx.accounts.owner.key;
-      journal_entry.title = title;
-      journal_entry.message = message;
+        require!(title.len() <= 50, ErrorCode::TitleTooLong);  
+        require!(message.len() <= 1000, ErrorCode::MessageTooLong);  
+        
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.owner = *ctx.accounts.owner.key;
+        journal_entry.title = title;
+        journal_entry.message = message;
 
-      Ok(())
+        Ok(())
     }
 
-    pub fn update_journal_entry(ctx: Context<UpdateEntry>, title: String, message: String) -> Result<()> {
-      let journal_entry = &mut ctx.accounts.journal_entry;
-      journal_entry.message = message;
+    pub fn update_journal_entry(ctx: Context<UpdateEntry>, _title: String, message: String) -> Result<()> {
+        require!(message.len() <= 1000, ErrorCode::MessageTooLong);  
+        
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.message = message;
 
-      Ok(())
+        Ok(())
     }
 
-    pub fn delete_journal_entry(ctx: Context<DeleteEntry>, title: String) -> Result<()> {
-      Ok(())
+    pub fn delete_journal_entry(_ctx: Context<DeleteEntry>, _title: String) -> Result<()> {
+        Ok(())
     }
 }
 
 #[derive(Accounts)]
 #[instruction(title: String)]
-
 pub struct CreateEntry<'info> {
-  #[account(
-    init,
-    seeds = [title.as_bytes(), owner.key().as_ref()],
-    bump,
-    space = 8 + JournalEntryState::INIT_SPACE,
-    payer = owner,
-  )]
-  pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(
+        init,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        space = 8 + 32 + 4 + 50 + 4 + 1000,  
+        payer = owner,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
 
-  #[account(mut)]
-  pub owner: Signer<'info>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
 
-  pub system_program: Program<'info, System>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 #[instruction(title: String)]
-
 pub struct UpdateEntry<'info> {
-  #[account(
-    mut,
-    seeds = [title.as_bytes(), owner.key().as_ref()],
-    bump,
-    realloc = 8 + JournalEntryState::INIT_SPACE,
-    realloc::payer = owner,
-    realloc::zero = true,
-  )]
-  pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        realloc = 8 + 32 + 4 + 50 + 4 + 1000, 
+        realloc::payer = owner,
+        realloc::zero = true,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
 
-  #[account(mut)]
-  pub owner: Signer<'info>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
 
-  pub system_program: Program<'info, System>,
-
+    pub system_program: Program<'info, System>,
 }
 
-#derive(Accounts)]
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        close = owner,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
 
 #[account]
-#[derive(InitSpace)]
 pub struct JournalEntryState {
-  pub owner:  Pubkey,
-  #[max_len = (50)]
-  pub title:  String,
-  #[max_len = (1000)]
-  pub message:  String,
+    pub owner: Pubkey,
+    pub title: String,  
+    pub message: String,  
 }
 
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The title exceeds the maximum length of 50 characters.")]
+    TitleTooLong,
+    #[msg("The message exceeds the maximum length of 1000 characters.")]
+    MessageTooLong,
+}
